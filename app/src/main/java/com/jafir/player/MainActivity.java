21e5aa1d561f16f20a94eabdcd172072bd54c767
jafir <522648467@qq.com>
2019-11-25 18:21:22 +0800

    切换so

--- app/src/main/java/com/jafir/player/MainActivity.java
+++ app/src/main/java/com/jafir/player/MainActivity.java
@@ -74,7 +74,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String INTENT_KEY_FIRST_FULL = "intent_key_first_full";
 
 
-    private String MOCK_PLAY_URL = "http://cctvalih5ca.v.myalicdn.com/live/cctv4_2/index.m3u8";
+//    private String MOCK_PLAY_URL = "http://cctvalih5ca.v.myalicdn.com/live/cctv4_2/index.m3u8";
+    private String MOCK_PLAY_URL = "rtsp://61.156.103.73:554/PLTV/88888888/224/3221226043/10000100000000060000000000286778_0.smil";
     //需要m3u8地址
     private String MOCK_DROP_URL = "http://cctvalih5ca.v.myalicdn.com/live/cctv4_2/index.m3u8";
     public static final String MOCK_TITLE = "播放标题";
